20c0f8db415485bc7464ac721ba273a7fb9c4f6f
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-04-25 07:37:34 -0300

    primeira atualização
    atualização do perfil pessoal

---
+++ SITE TEST/js/maquina.js
@@ -0,0 +1,12 @@
+function clicMenu(){
+    if (link.style.display=='block'){
+        link.style.display='none'
+    }else{
+        link.style.display='block'
+    }
+}
+function mudoutamanho(){
+    if(window.innerWidth>=900){
+        links.style.display='block'
+    }else{links.style.display='none'}
+}
